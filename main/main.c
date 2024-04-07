523027de0323ceb4364dbd443c336de223bc3346
Rafael <rafaelgp3@al.insper.edu.br>
2024-04-07 21:56:41 +0000

    mandando dados para python

--- main/main.c
+++ main/main.c
@@ -20,11 +20,21 @@ typedef struct adc {
     int val;
 } adc_t;
 
+void write_package(adc_t data) {
+    int val = data.val;
+    int msb = val >> 8;
+    int lsb = val & 0xFF;
+
+    uart_putc_raw(uart0, data.axis);
+    uart_putc_raw(uart0, lsb);
+    uart_putc_raw(uart0, msb);
+    uart_putc_raw(uart0, -1);
+}
+
 void x_task() {
 
     adc_init();
     adc_gpio_init(26);
-    // adc_select_input(0);
     adc_set_round_robin(0b00011);
 
     adc_t data_x;
@@ -33,7 +43,7 @@ void x_task() {
     while (true) {
 
         uint16_t result_x = adc_read();
-        data_x.val = (result_x - 2047) * 255 / 2047;
+        data_x.val = (result_x - 2047) * 255 / 2048;
         xQueueSend(xQueueAdc, &data_x, 0);
         vTaskDelay(pdMS_TO_TICKS(100));
     }
@@ -43,7 +53,6 @@ void y_task() {
 
     adc_init();
     adc_gpio_init(27);
-    // adc_select_input(1);
     adc_set_round_robin(0b00011);
 
     adc_t data_y;
@@ -52,19 +61,20 @@ void y_task() {
     while (true) {
 
         uint16_t result_y = adc_read();
-        data_y.val = (result_y - 2047) * 255 / 2047;
+        data_y.val = - (result_y - 2047) * 255 / 2047;
         xQueueSend(xQueueAdc, &data_y, 0);
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
 void uart_task(void *p) {
-    adc_t data;
+    adc_t data_to_send;
 
     while (1) {
-        if (xQueueReceive(xQueueAdc, &data, pdMS_TO_TICKS(10))) {
-            if (data.val < -30 || data.val > 30) {
-                printf("%d: %d\n", data.axis, data.val);
+        if (xQueueReceive(xQueueAdc, &data_to_send, pdMS_TO_TICKS(10))) {
+            if (data_to_send.val < -30 || data_to_send.val > 30) {
+                // printf("%d: %d\n", data.axis, data.val);
+                write_package(data_to_send);
             }
         }
     }
